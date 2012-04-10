0516fea8f5a9c81673cbda82beb230d9c49524bb
Isak <isak.renstrom@udk.se>
2012-04-10 20:40:23 +0200

    Argh

--- MyRobots/robots/umejug/Totoro.java
+++ MyRobots/robots/umejug/Totoro.java
@@ -3,26 +3,41 @@ package umejug;
 import java.awt.Color;
 import java.util.Random;
 
-import robocode.Robot;
+import robocode.AdvancedRobot;
 import robocode.ScannedRobotEvent;
 
-public class Totoro extends Robot {
+public class Totoro extends AdvancedRobot {
 
 	@Override
 	public void run() {
 		setColors(Color.DARK_GRAY, Color.GRAY, Color.WHITE);
+		setTurnRadarLeft(Double.POSITIVE_INFINITY);
+		setAdjustGunForRobotTurn(true);
+		setAdjustRadarForGunTurn(true);
+		setAdjustRadarForRobotTurn(true);
+
 		Random random = new Random();
 		while (true) {
-			ahead(random.nextDouble() * 50 + 20);
-			turnLeft(random.nextDouble() * 50);
+			setAhead(random.nextDouble() * 50 + 20);
+			turnLeft((random.nextDouble()) * 50 + 25);
 		}
 	}
 
 	@Override
 	public void onScannedRobot(ScannedRobotEvent event) {
+		// turnRadarRight(10);
 		double distance = event.getDistance();
-		if (distance > 150) {
+		// double targetHeading = event.getHeading() - 45;
+		// double gunHeading = getGunHeading();
+		// if (gunHeading < targetHeading) {
+		// turnGunLeft(gunHeading - targetHeading);
+		// } else {
+		// turnGunRight(targetHeading - gunHeading);
+		// }
+		if (distance > 300) {
 			fire(1);
+		} else if (distance > 150) {
+			fire(2);
 		} else {
 			fire(3);
 		}
