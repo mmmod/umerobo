a7d21e59cf2d835fd99361617f5ba75839a02f5a
Samuel Moritz <samuel.moritz@gmail.com>
2012-04-10 18:51:45 +0200

    the ultimate

---
+++ MyRobots/robots/umejug/SuperRobot.java
@@ -0,0 +1,35 @@
+package umejug;
+
+import robocode.HitWallEvent;
+import robocode.Robot;
+
+public class SuperRobot extends Robot {
+	
+	private boolean hideState = true;
+	private boolean turnState = false;
+	private double bearing;
+	private double fireStart = 2;
+	@Override
+	public void run() {
+		super.run();
+		while (true) {
+			if (hideState) {
+				ahead(50);
+			} else if (turnState) {
+				turnLeft(bearing);
+				turnState = false;
+			} else {
+				fire(fireStart);
+				fireStart = fireStart - 0.3;
+			}
+		}
+	}
+	
+	@Override
+	public void onHitWall(HitWallEvent event) {
+		hideState = false;
+		turnState = true;
+		bearing = event.getBearingDegrees();		
+	}
+
+}
